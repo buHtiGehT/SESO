627c97fcd5558432fcdfbe60036ceef010b311df
Smarshall19 <Smarshall19@shschools.org>
2015-12-23 22:33:41 -0800

    Fixed Page layout, changed coloring

--- Fade.js
+++ Fade.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-$("#FirstImage").slideDown();
+<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.3.min.js"></script>
+<script src="/jquery-1.11.3.min.js"></script>
+
+$(document).ready(Function() {
+                  
+                  
+                  
+                  
 });
